7d40364e4484572127d825ebb682b487f7c7d62c
fatahrahimi330 <138868867+fatahrahimi330@users.noreply.github.com>
2025-11-08 21:38:06 +0500

    Module 1-Face-Recognition

---
+++ Module 1-Face-Recognition/Module_1_Face_Recognition/face_recognition_nocomment.py
@@ -0,0 +1,33 @@
+# Importing Libraries
+import cv2
+
+# Loading the casCades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eyes_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+# Defining a function to do detection
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w] 
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eyes_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    return frame
+
+# Doing some Face Recognition with the webcame
+
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame) 
+    cv2.imshow('Video', canvas) 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+
+video_capture.release()
+cv2.destroyAllWindows() 
